1d8e4e964660f0c8ee0c61a8f7c003e1a1f86720
mudit91 <mudit91@umd.edu>
2014-04-05 15:17:40 -0400

    distance to location code
    code form internet for review and use

--- app/Controller/StationsController.php
+++ app/Controller/StationsController.php
@@ -47,7 +47,32 @@ class StationsController extends AppController {
 		}
 	}
 	
-	public function _distanceToLocation( $start_lat, $start_lon, $end_lat, $end_lon ) {
-		// add internet code
+	public function _distanceToLocation( $lat1, $lon1, $lat2, $lon2 ) {
+		// add internet code --> GeoDataSource.com (C) All Rights Reserved 2014		   		
+	    //function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+        //returns distance in miles by default
+	  $theta = $lon1 - $lon2;
+	  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+	  $dist = acos($dist);
+	  $dist = rad2deg($dist);
+	  $miles = $dist * 60 * 1.1515;
+		//$unit = strtoupper($unit);
+//remove these lines of you don't need kilometers/nautical miles
+		//	  if ($unit == "K") {
+		//		return ($miles * 1.609344);
+		//	  } else if ($unit == "N") {
+		//		  return ($miles * 0.8684);
+		//		} else
+       return $miles;
+		  }
 	}
+//test cases->
+	//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
+	//	echo distance(32.9697, -96.80322, 29.46786, -98.53506, "K") . " Kilometers<br>";
+	//echo distance(32.9697, -96.80322, 29.46786, -98.53506, "N") . " Nautical Miles<br>";
+
+	
+	
+	
+	}//here
 }
